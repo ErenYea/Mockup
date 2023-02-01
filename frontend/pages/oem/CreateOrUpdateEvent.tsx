f9d7d645d9a4c19d4e3cd1267844a349d32f82aa
ErenYea <drstoneluffy@gmail.com>
2023-02-02 03:06:47 +0500

    done with oem

---
+++ frontend/pages/oem/CreateOrUpdateEvent.tsx
@@ -0,0 +1,246 @@
+import React from "react";
+import { FormControl } from "baseui/form-control";
+import { Input } from "baseui/input";
+import { Textarea } from "baseui/textarea";
+import { Datepicker } from "baseui/datepicker";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalButton,
+  SIZE,
+  ROLE,
+} from "baseui/modal";
+import { data } from "./data";
+
+export default ({ onClose, isOpen, onSubmit }: any) => {
+  const [title, setTitle] = React.useState("");
+  const [model, setModel] = React.useState("");
+  // const [person, setPerson] = React.useState(event.person ? event.person : "");
+  // const [slots, setSlots] = React.useState<any>([event.start, event.end]);
+  const [selectedData, setSelectedData] = React.useState<any>([]);
+  const [personSelect, setPersonSelect] = React.useState<any>("");
+
+  const [modalModal, setModalModal] = React.useState<any>([
+    "Ford Motors",
+    "Nissan",
+    "Hyundai",
+    "Honda",
+    "Toyota",
+  ]);
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    var workshop = selectedData.filter((item) => item.id == personSelect)[0];
+    var carname = modalModal.filter(
+      (item) => item.slice(0, 3).toLowerCase() == model
+    )[0];
+    onSubmit({ car: parseInt(title), model: carname, workshop });
+  }
+  // const
+  const modelHandler = () => {
+    const value = document.getElementById("models").value;
+    console.log(value);
+    const filteredData = data;
+    filteredData.sort(
+      (a, b) => b.modaldata[value].performance - a.modaldata.toy.performance
+    );
+    setSelectedData(filteredData);
+
+    setModel(document.getElementById("models").value);
+  };
+  const personHandler = (e) => {
+    // if (e.target.querySelector("#name")) {
+    //   console.log(e.target.querySelector("#name").textContent);
+    // } else if (e.target.parentElement.querySelector("#name")) {
+    //   console.log(e.target.parentElement.querySelector("#name").textContent);
+    // } else {
+    //   console.log(
+    //     e.target.parentElement.parentElement.querySelector("#name").textContent
+    //   );
+    // }
+    const key = e.target.getAttribute("data-key");
+    console.log("key", key);
+    // console.log(document.querySelector(`main[data-key='${key}']`));
+    if (document.querySelector(`main[data-key='${key}']`)) {
+      document
+        .querySelector(`main[data-key='${key}']`)
+        .classList.add("bg-gray-900");
+    }
+    setPersonSelect(key);
+    console.log(e);
+    // .target.parentNode.querySelector("div")
+  };
+
+  return (
+    <Modal
+      onClose={onClose}
+      closeable
+      isOpen={isOpen}
+      animate
+      autoFocus
+      size={SIZE.default}
+      role={ROLE.dialog}
+      overrides={{
+        Root: {
+          style: {
+            zIndex: 99999,
+          },
+        },
+      }}
+    >
+      <form onSubmit={handleSubmit}>
+        <ModalBody>
+          <FormControl label="Set Number of Cars">
+            <Input
+              id="input-id"
+              value={title}
+              type="number"
+              onChange={(event) => setTitle(event.currentTarget.value)}
+              required
+            />
+          </FormControl>
+
+          <FormControl label="Select Model">
+            <>
+              <select
+                id="models"
+                onChange={modelHandler}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option selected={model != "" ? false : true} disabled>
+                  Choose a Model
+                </option>
+                {modalModal.map((item, index) => (
+                  <option
+                    key={index}
+                    value={item.slice(0, 3).toLowerCase()}
+                    selected={
+                      model != ""
+                        ? model == item.slice(0, 3).toLowerCase()
+                          ? true
+                          : false
+                        : false
+                    }
+                  >
+                    {item}
+                  </option>
+                ))}
+                {/* <option value="US">United States</option>
+                <option value="CA">Canada</option>
+                <option value="FR">France</option>
+                <option value="DE">Germany</option> */}
+              </select>
+            </>
+          </FormControl>
+
+          <FormControl label="Select Workshop">
+            <>
+              <div className="flex border rounded justify-center border-gray-800 h-[300px] w-full bg-gray-600">
+                {model == ""
+                  ? ""
+                  : selectedData?.map((i, ind) => (
+                      <main
+                        key={ind}
+                        onClick={(e) => personHandler(e)}
+                        data-key={i.id}
+                        className={
+                          "flex w-[130px] flex-col border border-gray-900 rounded items-center hover:bg-gray-900  hover:cursor-pointer active:bg-gray-900" +
+                          (personSelect != ""
+                            ? personSelect == i.id
+                              ? "bg-gray-900"
+                              : ""
+                            : "")
+                        }
+                      >
+                        {/* <img
+                          data-key={i.id}
+                          src={i.img}
+                          alt=""
+                          className="w-[130px] h-[100px] rounded"
+                        /> */}
+                        <div
+                          data-key={i.id}
+                          id="name"
+                          className=" text-purple-600 text-bold text-lg"
+                        >
+                          {i.name}
+                        </div>
+                        <div
+                          className=" text-red-600 text-base h-[24px]"
+                          ddata-key={i.id}
+                        >
+                          {ind == 0 ? "Recomended*" : ""}
+                        </div>
+                        <div
+                          className="flex flex-col items-center border-gray-400"
+                          data-key={i.id}
+                        >
+                          <div
+                            className="text-white text-[14px] text-center"
+                            data-key={i.id}
+                          >
+                            Performance:{" "}
+                            <span
+                              className=" text-yellow-300 text-center w-full"
+                              data-key={i.id}
+                            >
+                              {i.modaldata[model].performance}/10
+                            </span>
+                          </div>
+                          <div
+                            className="text-white text-[14px] text-center"
+                            data-key={i.id}
+                          >
+                            Material Present:{" "}
+                            <span
+                              className=" text-yellow-300 text-center w-full"
+                              data-key={i.id}
+                            >
+                              {i.modaldata[model].material ? "Yes" : "No"}
+                            </span>
+                          </div>
+                          <div
+                            className="text-white text-[14px] text-center"
+                            data-key={i.id}
+                          >
+                            Workload:{" "}
+                            <span
+                              className=" text-yellow-300 text-center w-full"
+                              data-key={i.id}
+                            >
+                              {i.workload} cars this week
+                            </span>
+                          </div>
+                        </div>
+                      </main>
+                    ))}
+              </div>
+              {/* <select
+                disabled={model == ""}
+                id="persons"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option selected disabled>
+                  Choose a Person
+                </option>
+
+                <option value="US">United States</option>
+                <option value="CA">Canada</option>
+                <option value="FR">France</option>
+                <option value="DE">Germany</option>
+              </select> */}
+            </>
+          </FormControl>
+        </ModalBody>
+        <ModalFooter>
+          <ModalButton type="button" onClick={onClose}>
+            Cancel
+          </ModalButton>
+          <ModalButton>Okay</ModalButton>
+        </ModalFooter>
+      </form>
+    </Modal>
+  );
+};
--- frontend/pages/oem/index.tsx
+++ frontend/pages/oem/index.tsx
@@ -4,43 +4,127 @@ import PageTitle from "components/UiElements/PageTitle/PageTitle";
 import ApexChart from "components/UiElements/ApexChart/ApexChart";
 import Head from "next/head";
 import LineBar from "./lineGraph";
+import CreateOrUpdateEvent from "./CreateOrUpdateEvent";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalButton,
+  SIZE,
+  ROLE,
+} from "baseui/modal";
+import Container from "components/UiElements/Container/Container";
+import { Block } from "baseui/block";
 
 // type Props = {};
 
 const index = () => {
+  const [event, setEvent] = useState(false);
+  const [works, setWorks] = useState([]);
+  const [isOpen, setIsOpen] = React.useState(false);
+  function onSubmit(value: any) {
+    setIsOpen(false);
+    console.log(value);
+    setWorks([...works, value]);
+  }
+  const handleClick = () => {
+    console.log("hamza");
+    setEvent(true);
+    setIsOpen(true);
+  };
+  function close() {
+    setIsOpen(false);
+    setEvent(null);
+  }
   var data = [
     {
       name: "Ford",
-      vehiclesSold:[110, 113, 167, 112, 132, 122, 132,124,111,144,114,127,143],
-      sunroof:[27,44,36,42,32,23,36,37,39,24,22,41,43],
+      vehiclesSold: [
+        110,
+        113,
+        167,
+        112,
+        132,
+        122,
+        132,
+        124,
+        111,
+        144,
+        114,
+        127,
+        143,
+      ],
+      sunroof: [27, 44, 36, 42, 32, 23, 36, 37, 39, 24, 22, 41, 43],
     },
     {
       name: "Nissan",
-      vehiclesSold:[92, 98, 110, 112, 142, 91, 88 ,43 ,67 ,76 ,110 ,131,95],
-      sunroof:[23,42,39,32,48,49,41,36,40,31,29,52,46],
+      vehiclesSold: [92, 98, 110, 112, 142, 91, 88, 43, 67, 76, 110, 131, 95],
+      sunroof: [23, 42, 39, 32, 48, 49, 41, 36, 40, 31, 29, 52, 46],
     },
     {
       name: "Hyundai",
-      vehiclesSold:[143 , 123, 142, 165, 134, 145, 99,101,114,154,111,138,139],
-      sunroof:[25,19,33,41,37,24,36,23,17,42,23,46,35],
+      vehiclesSold: [
+        143,
+        123,
+        142,
+        165,
+        134,
+        145,
+        99,
+        101,
+        114,
+        154,
+        111,
+        138,
+        139,
+      ],
+      sunroof: [25, 19, 33, 41, 37, 24, 36, 23, 17, 42, 23, 46, 35],
     },
     {
       name: "Honda",
-      vehiclesSold:[134, 171, 142, 145, 132, 133, 164, 143, 115, 134, 121, 165,169],
-      sunroof:[34,56,43,51,34,29,42,37,39,25,16,15,13],
+      vehiclesSold: [
+        134,
+        171,
+        142,
+        145,
+        132,
+        133,
+        164,
+        143,
+        115,
+        134,
+        121,
+        165,
+        169,
+      ],
+      sunroof: [34, 56, 43, 51, 34, 29, 42, 37, 39, 25, 16, 15, 13],
     },
     {
       name: "Toyota",
-      vehiclesSold:[89, 67, 150, 111, 101, 128, 154,131,141,113,88 ,141,96],
-      sunroof:[45,41,36,27,29,21,31,37,25,36,31,14,28],
+      vehiclesSold: [
+        89,
+        67,
+        150,
+        111,
+        101,
+        128,
+        154,
+        131,
+        141,
+        113,
+        88,
+        141,
+        96,
+      ],
+      sunroof: [45, 41, 36, 27, 29, 21, 31, 37, 25, 36, 31, 14, 28],
     },
-
   ];
 
   useEffect(() => {
-    document.getElementsByTagName('nav')[0].style.display='None'
-  },[]);
-  
+    document.getElementsByTagName("nav")[0].style.display = "None";
+  }, []);
+
   const [Index, setIndex] = useState(0);
   function showInfo() {
     var element: HTMLInputElement = document.getElementById("dropdown");
@@ -89,7 +173,15 @@ const index = () => {
         size: 1,
       },
       xaxis: {
-        categories: ["Jul' 22","Aug' 22", "Sept' 22", "Oct' 22", "Nov' 22", "Dec' 22", "Jan' 23"],
+        categories: [
+          "Jul' 22",
+          "Aug' 22",
+          "Sept' 22",
+          "Oct' 22",
+          "Nov' 22",
+          "Dec' 22",
+          "Jan' 23",
+        ],
         title: {
           text: "Month",
         },
@@ -119,41 +211,99 @@ const index = () => {
       <div className="w-full flex flex-col items-center justify-center">
         <div className="w-full flex-row flex justify-center items-center z-10 mb-10">
           <label className="m-2 text-xl font-semibold text-customDarkBlue">
-                      Select Model
+            Select Model
           </label>
-                    <select
-                      id="dropdown"
-                      onChange={showInfo}
-                      className="w-1/4 p-2.5 text-gray-700 font-bold bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-                    >
-                      {data.map((val, ind) => {
-                        return (
-                          <option value={ind} key={ind} className="font-bold">
-                            {val.name}
-                          </option>
-                        );
-                      })}
-                    </select>
+          <select
+            id="dropdown"
+            onChange={showInfo}
+            className="w-1/4 p-2.5 text-gray-700 font-bold bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+          >
+            {data.map((val, ind) => {
+              return (
+                <option value={ind} key={ind} className="font-bold">
+                  {val.name}
+                </option>
+              );
+            })}
+          </select>
         </div>
         <div className="w-full flex flex-col justify-center items-center">
           <span className="text-2xl font-black">Vehicles Sold</span>
-          <LineBar args={data[Index]['vehiclesSold']}/>
+          <LineBar args={data[Index]["vehiclesSold"]} />
           <div className="w-[83%] flex flex-row items-start justify-end -mt-[44px]">
             <span className="text-sm text-black">Prediction</span>
           </div>
         </div>
         <div className="w-full flex flex-col justify-center items-center mt-4">
           <span className="text-2xl font-black">Sunroof Ordering</span>
-          <LineBar args={data[Index]['sunroof']}/>
+          <LineBar args={data[Index]["sunroof"]} />
           <div className="w-[83%] flex flex-row items-start justify-end -mt-[44px]">
             <span className="text-sm text-black">Prediction</span>
           </div>
         </div>
-      
-        
+        <Container>
+          <Block className={"flex items-end justify-end"}>
+            <ModalButton onClick={handleClick} className="right-0">
+              Order Job
+            </ModalButton>
+          </Block>
+          <Block
+            marginTop={"20px"}
+            className="border border-gray-700 h-full rounded bg-gray-200 "
+          >
+            <div className="flex flex-col p-2 min-h-[400px]">
+              <div className="text-bold text-xl border p-2 text-white bg-slate-800 w-fit">
+                Active Jobs
+              </div>
+              {works?.map((i) => (
+                <div
+                  className="flex justify-evenly w-full border border-gray-800 bg-slate-400 rounded-lg mt-2"
+                  key={i.workshop.id}
+                >
+                  <div className="text-base border-gray-700 border p-2 rounded bg-white">
+                    This is {i.workshop.name}
+                  </div>
+                  <div className="text-base p-2 border border-gray-700 rounded bg-white">
+                    This is the job of {i.car} {i.model} cars
+                  </div>
+                </div>
+              ))}
+              {/* <div className="flex justify-evenly w-full border border-gray-800 bg-slate-400 rounded-lg mt-2">
+            <div className="text-base border-gray-700 border p-2 rounded bg-white">
+              This is workshop 1
+            </div>
+            <div className="text-base p-2 border border-gray-700 rounded bg-white">
+              This is the job of 100 cars
+            </div>
+          </div>
+          <div className="flex justify-evenly w-full border border-gray-800 bg-slate-400 rounded-lg mt-2">
+            <div className="text-base border-gray-700 border p-2 rounded bg-white">
+              This is workshop 1
+            </div>
+            <div className="text-base p-2 border border-gray-700 rounded bg-white">
+              This is the job of 100 cars
+            </div>
+          </div>
+          <div className="flex justify-evenly w-full border border-gray-800 bg-slate-400 rounded-lg mt-2">
+            <div className="text-base border-gray-700 border p-2 rounded bg-white">
+              This is workshop 1
+            </div>
+            <div className="text-base p-2 border border-gray-700 rounded bg-white">
+              This is the job of 100 cars
+            </div>
+          </div> */}
+            </div>
+          </Block>
+          {event && (
+            <CreateOrUpdateEvent
+              onClose={close}
+              isOpen={isOpen}
+              event={event}
+              onSubmit={(value: any) => onSubmit(value)}
+            />
+          )}
+        </Container>
       </div>
-      
-
     </>
   );
 };
