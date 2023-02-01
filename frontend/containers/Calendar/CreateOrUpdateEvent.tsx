09bf5496dc303ac0e94f106dcf8ec719dbd777e3
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-02-01 20:53:05 +0500

    select options added

--- frontend/containers/Calendar/CreateOrUpdateEvent.tsx
+++ frontend/containers/Calendar/CreateOrUpdateEvent.tsx
@@ -14,7 +14,8 @@ import {
 } from "baseui/modal";
 export default ({ onClose, isOpen, onSubmit, event }: any) => {
   const [title, setTitle] = React.useState(event.title ? event.title : "");
-  const [desc, setDescription] = React.useState(event.desc ? event.desc : "");
+  const [model, setModel] = React.useState("");
+  const [person, setPerson] = React.useState(event.person ? event.person : "");
   const [slots, setSlots] = React.useState<any>([event.start, event.end]);
   const [starttime, setStartTime] = React.useState<any>(
     event.starttime ? event.starttime : ""
@@ -25,8 +26,13 @@ export default ({ onClose, isOpen, onSubmit, event }: any) => {
 
   function handleSubmit(e: any) {
     e.preventDefault();
-    onSubmit({ ...event, title, desc, slots, starttime, endtime });
+    onSubmit({ ...event, title, model, person, slots, starttime, endtime });
   }
+
+  const modelHandler = () => {
+    setModel(document.getElementById('models').value)
+  }
+
   return (
     <Modal
       onClose={onClose}
@@ -55,13 +61,31 @@ export default ({ onClose, isOpen, onSubmit, event }: any) => {
               required
             />
           </FormControl>
-          <FormControl label="Set Description">
-            <Textarea
-              id="textarea-id"
-              value={desc}
-              onChange={(event) => setDescription(event.currentTarget.value)}
-            />
+
+          <FormControl label="Select Model">
+            <>
+              <select id="models" onChange={modelHandler} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected disabled>Choose a Model</option>
+                <option value="US">United States</option>
+                <option value="CA">Canada</option>
+                <option value="FR">France</option>
+                <option value="DE">Germany</option>
+              </select>
+            </>
           </FormControl>
+
+          <FormControl label="Select Person">
+            <>
+              <select disabled={model == ''} id="persons" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected disabled>Choose a Person</option>
+                <option value="US">United States</option>
+                <option value="CA">Canada</option>
+                <option value="FR">France</option>
+                <option value="DE">Germany</option>
+              </select>
+            </>
+          </FormControl>
+
           <FormControl label="Set Date">
             <Datepicker
               range
